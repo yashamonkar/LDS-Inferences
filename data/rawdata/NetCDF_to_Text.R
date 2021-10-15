64485c0aa6ebfbe358507cf86ae4060609b08c00
yashamonkar <39978782+yashamonkar@users.noreply.github.com>
2021-10-15 18:08:05 -0400

    Update NetCDF_to_Text.R

--- data/rawdata/NetCDF_to_Text.R
+++ data/rawdata/NetCDF_to_Text.R
@@ -10,36 +10,10 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(ncdf4) 
 
 
-#______________________________________________________________________________#
-#Wind Power
-name <- c("ERCOT_daily_WP_0_5_deg.nc")
-nc_data <- nc_open(name)
-print(nc_data) 
-lat_lon_index <- ncvar_get(nc_data, "lat_lon_index")
-t <- ncvar_get(nc_data, "time")
-WP_CF <- ncvar_get(nc_data, "WP") 
-WP_CF <- t(WP_CF) #Shifting to a correct format. 
-nc_close(nc_data) #Closing the Netcdf file. 
-write.table(WP_CF, "ERCOT_Wind_Power_Daily.txt", sep=" ")
-
-
-#______________________________________________________________________________#
-#Solar
-name <- c("ERCOT_daily_RAD_0_5_deg.nc")
-nc_data <- nc_open(name)
-print(nc_data) 
-lat_lon_index <- ncvar_get(nc_data, "lat_lon_index")
-t <- ncvar_get(nc_data, "time")
-ssrd <- ncvar_get(nc_data, "ssrd") 
-ssrd <- t(ssrd) #Shifting to a correct format. 
-nc_close(nc_data) #Closing the Netcdf file. 
-write.table(ssrd, "ERCOT_Solar_Rad_Daily.txt", sep=" ")
-
-
 
 #______________________________________________________________________________#
 #Wind Power - Extended
-name <- c("ERCOT_daily_WP_CF_0_5_deg_1950_2020.nc")
+name <- c("ERCOT_daily_WP_CF_0_5_deg.nc")
 nc_data <- nc_open(name)
 print(nc_data) 
 lat_lon_index <- ncvar_get(nc_data, "lat_lon_index")
@@ -47,12 +21,12 @@ t <- ncvar_get(nc_data, "time")
 WP_CF <- ncvar_get(nc_data, "WP CF") 
 WP_CF <- t(WP_CF) #Shifting to a correct format. 
 nc_close(nc_data) #Closing the Netcdf file. 
-write.table(WP_CF, "ERCOT_Wind_Power_Daily_Extended.txt", sep=" ")
+write.table(WP_CF, "ERCOT_Wind_CF_Daily.txt", sep=" ")
 
 
 #______________________________________________________________________________#
 #Solar - Extended
-name <- c("ERCOT_daily_SP_CF_0_5_deg_1950_2020.nc")
+name <- c("ERCOT_daily_SP_CF_0_5_deg.nc")
 nc_data <- nc_open(name)
 print(nc_data) 
 lat_lon_index <- ncvar_get(nc_data, "lat_lon_index")
@@ -60,4 +34,4 @@ t <- ncvar_get(nc_data, "time")
 ssrd <- ncvar_get(nc_data, "SP CF") 
 ssrd <- t(ssrd) #Shifting to a correct format. 
 nc_close(nc_data) #Closing the Netcdf file. 
-write.table(ssrd, "ERCOT_Solar_Rad_Daily_Extended.txt", sep=" ")
+write.table(ssrd, "ERCOT_Solar_CF_Daily.txt", sep=" ")
